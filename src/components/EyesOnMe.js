a794cebd8b22ca9abf51df6b8d163edfb2e13ed9
rhaynes3k <rhaynes3k@gmail.com>
2022-11-04 14:05:47 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+// Code EyesOnMe Component Here
+
+function EyesOnMe () {
+
+  function onFocus () {
+    console.log('Good!')
+  }
+
+  function onBlur() {
+    console.log('Hey! Eyes on me!')
+  }
+
+  return(
+    <input type='button' value='Eyes on me' onFocus={onFocus} onBlur={onBlur}/>
+  )
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+  const handlePassword = () => {
+    console.log('Entering password...')
+  }
     return (
-        <div></div>
+        <div>
+          <input type='password' onChange={handlePassword}/>
+        </div>
     )
 }
 
